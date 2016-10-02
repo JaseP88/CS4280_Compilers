7d907f6693a92f5031ab54ed30d88301ff1b57d0
Jason Pham <jasep88@gmail.com>
2016-10-02 02:09:53 -0500

    added driver header file

---
+++ P1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "driver.h"
+#include "token.h"
+
+
+int main () {
+
+	//table[8][1] = 90;
+	printf("%d\n",table[0][2]);
+
+
+
+return 0;
+}
--- P1/token.h
+++ P1/token.h
@@ -1,114 +1,52 @@
 #ifndef TOKEN_H
 #define TOKEN_H
 
-#define keyword_ 14	//define how many keywords there are
-#define relationOp_ 6
-#define otherOp_ 8
-#define delim_ 9
-#define token_ 6
+#define tokens_ 40	//define how many tokens there are
 
 
 /* enumeration of all the tokenIDs */
-enum tokenID {
-	EofTk,
-	keywordTk,
-	idenTk,
-	relationTk,
-	delimTk,
-	//digiTk,	//digit token Id
-	opTk	//operator token
-};
+typedef enum tokenID {
+	Eof_Tk, 
+	Identifier_Tk,
+	IntLiteral_Tk,
 
-char *tk_str[token_] = {"End Of File", "Keyword", 
-	"Identifier", "Relational", "Delimiter" "Operator"};
-	
+	/* Keyword Tokens */
+	Bgn_Tk, End_Tk, Strt_Tk, Stp_Tk, Iff_Tk, Loop_Tk,
+	Void_Tk, Var_Tk, Int_Tk, Call_Tk, Return_Tk,
+	Scan_Tk, Prnt_Tk, Prg_Tk,
 
+	/* Operator Tokens */
+	Cln_Tk, DblEq_Tk, Plus_Tk, Minus_Tk, Ast_Tk, 
+	BkSlash_Tk, Amper_Tk, Prcnt_Tk,
 
-/* lower level depth of tokenID for keywords */
-enum keywordtk_inst {
-	Bgn,
-	End,
-	Strt,
-	Stp,
-	Iff,
-	Loop,
-	Void,
-	Var,
-	Int,
-	Call,
-	Return,
-	Scan,
-	Prnt,
-	Prg
-};
+	/* Relationsal Operators */
+	Eq_Tk, Lt_tk, Gt_tk, DnEq_Tk, Lt_Eq_Tk, Gt_Eq_Tk,
 
-/* string array to pair keyword instance */
-char *keywordtk_Str[keyword_] = {"Begin", "End", "Start",
-	"Stop", "Iff", "Loop", "Void", "Var", "Int", "Call", 
-	"Return", "Scan", "Print", "Program"};
+	/* Delimiters Tokens */
+	Period_Tk, LftParen_Tk, RgtParen_Tk, Comma_Tk,
+	LftBrace_Tk, RgtBrace_Tk, SemiColon_Tk, 
+	LftBracket_Tk, RgtBracket_Tk
+} tokenID;
 
+char *tokenNames[tokens_] = {"End of File", "Identifier", 
+	"Numbers", 
 
+	"Begin Keyword", "End Keyword", "Start Keyword",
+	"Stop Keyword", "Iff Keyword", "Loop Keyword", 
+	"Void Keyword", "Var Keyword", "Int Keyword", 
+	"Call Keyword", "Return Keyword", "Scan Keyword",
+	"Print Keyword", "Program Keyword", 
 
+	"Colon Operator", "Double Equal Operator", "Plus Operator",
+	"Minus Operator", "Asterisk Operator", "Backslash Operator",
+	"Ampersand Operator", "Percent Operator",
 
+	"Equal", "Less Than", "Greater Than", "Does Not Equal",
+	"Less Than Or Equal", "Greater Than Or Equal",
 
-
-enum relationtk_inst {
-	Eqtk, 
-	Lttk,
-	Gttk,
-	Dnetk,
-	Lt_Eqtk,
-	Gt_Eqtk
-};
-
-char *relationtk_Str[relationOp_] = {"Equal", "LessThan", 
-	"GreaterThan", "DoesNotExist", "LessOrEqual",
-	"GreatOrEqual"};
-
-
-
-
-
-
-enum optk_inst {
-	DblEq,
-	Col,
-	Plus,
-	Minus,
-	Ast,
-	BkSlash,
-	Amper,
-	Prcnt
-};
-
-char *optk_Str[otherOp_] = {"DoubleEqual", "Colon", "Plus",
-	"Minus", "Asterisk", "BackSlash", "Ampersand", "Percent"};
-
-
-
-
-
-enum delimtk_inst {
-	Prd,
-	Lparen,
-	Rparen,
-	Com,
-	LBrkt,
-	RBrkt,
-	Semi,
-	LBrc,
-	RBrc
-};
-
-char *delimtk_Str[delim_] = {"Period", "LeftParentheses",
-	"RightParentheses", "Comma", "LeftBracket",
-	"RightBracket", "Semicolon", "LeftBrace", "RightBrace"};
-
-
-
-
-
-
+	"Period", "Left Parentheses", "Right Parentheses", 
+	"Comma", "Left Brace", "Right Brace", "Semicolon",
+	"Left Bracket", "Right Bracket"};
 
 
 /* structure of the token */
